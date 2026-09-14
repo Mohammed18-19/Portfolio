a27cb7bc64f26b7a694b4e582c73870d81bca3b7
Mohammed <aintomar.mohamed19@gmail.com>
2026-09-14 15:26:02 +0100

    Fix 3D laptop camera positioning

--- components/canvas/Computers.jsx
+++ components/canvas/Computers.jsx
@@ -67,8 +67,8 @@ const Computers = ({ isMobile }) => {
 
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 1.0}
+        position={isMobile ? [0, -1.5, 0] : [0, -1.5, 0]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -105,7 +105,7 @@ const ComputersCanvas = () => {
       frameloop="demand"
       shadows
       dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [0, 0, 12], fov: 35 }}
       gl={{ preserveDrawingBuffer: true, antialias: true }}
       style={{ width: "100%", height: "100%" }}
     >
